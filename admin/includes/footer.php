59ea57c6fb9c41b9bef62fa5f450aa6bb70592b5
Robin Caulier <robincaulier3@gmail.com>
2025-01-16 10:56:42 +0100

    beginclasses

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -1,4 +1,3 @@
-
 <footer>
     <div class="footer clearfix mb-0 text-muted">
         <div class="float-start">
@@ -6,18 +5,17 @@
         </div>
         <div class="float-end">
             <p>Crafted with <span class="text-danger"><i class="bi bi-heart-fill icon-mid"></i></span>
-by <a href="https://saugi.me">Saugi</a></p>
+                by <a href="https://saugi.me">Saugi</a></p>
         </div>
     </div>
 </footer>
-        </div>
-    </div>
-    <script src="assets/static/js/components/dark.js"></script>
-    <script src="assets/extensions/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+</div>
+</div>
+<script src="assets/static/js/components/dark.js"></script>
+<script src="assets/extensions/perfect-scrollbar/perfect-scrollbar.min.js"></script>
 
 
-    <script src="assets/compiled/js/app.js"></script>
-
+<script src="assets/compiled/js/app.js"></script>
 
 
 <!-- Need: Apexcharts -->
